39d63fd84451cfdd46343e2c1e1e7b9c131f644c
Naman Choudhary <appleinc2341@gmail.com>
2023-02-15 11:13:39 +0530

    first commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  coud << "My name is Naman Choudhary, SRN PES2UG20CS209\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
